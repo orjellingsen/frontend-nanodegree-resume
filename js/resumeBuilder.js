ad4ba74f66805a7305f739f5e91c85ca50f06aed
orjellingsen <orjanell@gmail.com>
2015-10-31 14:20:57 +0100

    Added name/role and commented previous lessons

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,16 +2,23 @@
 
 // [string].replace([old],[new])
 
-var email ="orjanell@gmail.com";
+// var email ="orjanell@gmail.com";
 
-var newEmail = "orjellingsen@gmail.com";
+// var newEmail = "orjellingsen@gmail.com";
 
-console.log(email);
-console.log(newEmail);
+// console.log(email);
+// console.log(newEmail);
 
-var awesomeThoughts = "I am Ørjan and I am AWESOME!"
+// var awesomeThoughts = "I am Ørjan and I am AWESOME!"
 
-var funThoughts =
-	awesomeThoughts.replace("AWESOME!","FUN");
+// var funThoughts =
+// 	awesomeThoughts.replace("AWESOME!","FUN");
 
-$("#main").append(funThoughts);
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Ørjan Ellingsen");
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
