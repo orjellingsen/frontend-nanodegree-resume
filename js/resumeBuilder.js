ea714dcb830e7412231c541a326fe23c191d36fe
orjellingsen <orjanell@gmail.com>
2015-10-31 13:59:29 +0100

    Added script from lesson about string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,9 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("Ørjan");
+
+ var awesomeThoughts = "I am Ørjan and I am AWESOME!"
+ var funThoughts =
+ 		awesomeThoughts.replace("AWESOME!","FUN");
+
+ $("#main").append(funThoughts);
