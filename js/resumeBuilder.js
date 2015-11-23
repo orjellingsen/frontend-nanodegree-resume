45278a902ac0302890a127b2f3efa2789865dfea
orjellingsen <orjanell@gmail.com>
2015-11-23 22:57:44 +0100

    Added work and bio loops to format and display the information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,7 +8,7 @@ var bio = {
 		"github" : "orjellingsen",
 		"twitter" : "@johndoe",
 		"location" : "Bergen, Norway"
-	},	
+	},
 	"skills" : ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
 	"bioPic" : "images/fry.jpg"
 };
@@ -32,12 +32,14 @@ var work = {
 }
 var projects = {
 	"projects" : [
-		"title" : "Portfolio",
-		"dates" : "2015",
-		"description" : "Portfolio project for Udacity"
-		"images" : [
-			"http://codepen.io/deve/pen/BoYqWz"
-		]
+		{
+			"title" : "Portfolio",
+			"dates" : "2015",
+			"description" : "Portfolio project for Udacity",
+			"images" : [
+				"http://codepen.io/deve/pen/BoYqWz"
+			]
+		}
 	]
 }
 var education = {
@@ -46,11 +48,11 @@ var education = {
 			"name" : "U-Pihl Videregående Skole",
 			"city" : "Bergen",
 			"degree" : "VGS",
-			"major" : "Allmen",
+			"major" : "Allmenn",
 			"dates" : 2005,
 			"url" : "http://skole.hfk.no/upihlvgs"
 		}
-	]
+	],
 	"onlineCourses" : [
 		{
 			"name" : "Udacity",
@@ -60,4 +62,49 @@ var education = {
 			"url" : "www.udacity.com"
 		}
 	]
+}
+
+// Header
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderName.replace("%data%", bio.role);
+$("#header").append(formattedRole);
+
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+$("#header").append(formattedWelcomeMsg);
+
+// BioPic
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedBioPic);
+
+// Skills
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	for (var i = 0; i < bio.skills.length; i += 1) {
+		var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#header").append(formattedSkills);
+	}
+}
+
+// Work
+for (job in work.jobs) {
+	// create new div for work experience
+	$("#workExperience").append(HTMLworkStart);
+
+	// concat employer and title
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	
+	$(".work-entry:last").append(formattedEmployerTitle);
+	
+	var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedWorkDates);
+
+	var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	$(".work-entry:last").append(formattedWorkLocation);
+
+	var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedWorkDescription);
 }
