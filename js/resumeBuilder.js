9114c33e924df32f68da9babd4b4216a87160af9
orjellingsen <orjanell@gmail.com>
2015-11-23 21:25:59 +0100

    Cleared all previous code, added objects for bio, work, projects and education.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,30 +1,63 @@
-// $("#main").append("Ørjan");
-
-// [string].replace([old],[new])
-
-// var email ="orjanell@gmail.com";
-
-// var newEmail = "orjellingsen@gmail.com";
-
-// console.log(email);
-// console.log(newEmail);
-
-// var awesomeThoughts = "I am Ørjan and I am AWESOME!"
-
-// var funThoughts =
-// 	awesomeThoughts.replace("AWESOME!","FUN");
-
-// $("#main").append(funThoughts);
-
-var name = "Ørjan Ellingsen"
-var role = "Front-End Web Developer"
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var skills =
-["awesomeness", "programming", "teaching", "JS"];
+var bio = {
+	"name" : "Ørjan Ellingsen",
+	"role" : "Front-End Web Developer",
+	"welcomeMsg" : "lorem ipsum dolor sit amet etc etc etc.",
+	"contacts" : {
+		"mobile": "111 22 333",
+		"email" : "orjellingsen@gmail.com",
+		"github" : "orjellingsen",
+		"twitter" : "@johndoe",
+		"location" : "Bergen, Norway"
+	},	
+	"skills" : ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
+	"bioPic" : "images/fry.jpg"
+};
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Paradis Barnehage",
+			"title" : "Assistent",
+			"location" : "Bergen",
+			"dates" : "2008 - 2011",
+			"description" : "Barnehage"
+		},
+		{
+			"employer" : "Kidsa Barnehager",
+			"title" : "Assistent",
+			"location" : "Bergen",
+			"dates" : "2011 - Present",
+			"description" : "Friluftsbarnehage"
+		}
+	]
+}
+var projects = {
+	"projects" : [
+		"title" : "Portfolio",
+		"dates" : "2015",
+		"description" : "Portfolio project for Udacity"
+		"images" : [
+			"http://codepen.io/deve/pen/BoYqWz"
+		]
+	]
+}
+var education = {
+	"schools" : [
+		{
+			"name" : "U-Pihl Videregående Skole",
+			"city" : "Bergen",
+			"degree" : "VGS",
+			"major" : "Allmen",
+			"dates" : 2005,
+			"url" : "http://skole.hfk.no/upihlvgs"
+		}
+	]
+	"onlineCourses" : [
+		{
+			"name" : "Udacity",
+			"city" : "Online",
+			"degree" : "Front End Web Development Nanodegree",
+			"dates" : 2015,
+			"url" : "www.udacity.com"
+		}
+	]
+}
