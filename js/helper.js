51eec866c10da1e7610433655deee19757387280
orjellingsen <orjanell@gmail.com>
2015-11-30 21:04:07 +0100

    Added navbar, cleaned up css

--- js/helper.js
+++ js/helper.js
@@ -41,7 +41,7 @@ var HTMLprojectStart = '<div class="project-entry col-md-6"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%" class="img-responsive">';
+var HTMLprojectImage = '<img src="%data%" class="project-image img-responsive">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
